31ecc115d7d6752a75f37120eb80b6787b8c2e77
APOORVA456 <apoorva.8765@gmail.com>
2020-08-26 13:12:10 +0530

    4 files modified, 4 added

---
+++ src/app/country-gen/country-gen.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import {Injectable} from '@angular/core'
+@Component({
+  selector: 'app-country-gen',
+  templateUrl: './country-gen.component.html',
+  styleUrls: ['./country-gen.component.css']
+})
+export class CountryGenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/freespi.ts
@@ -0,0 +1,3 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import {Injectable} from '@angular/core'
